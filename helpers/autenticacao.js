17ce97bf08b8956b11fa27ef73d6eed83cd5d073
Leandro <Lgbissi@gmail.com>
2025-07-27 16:26:00 +0100

    Adicionando fixtures para gerir os corpos das requisicoes

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,18 +1,18 @@
-import request from 'supertest';
+const request = require('supertest'); //serve para interagir com a API
+const postLogin = require('../fixtures/postLogin.json'); //importa o arquivo JSON com os dados de login
 
 const obterToken = async (usuario, senha) =>{
+    const bodyLogin = {...postLogin}
+
     const respostaLogin = await request (process.env.BASE_URL) 
                 .post('/login')
                 .set('Content-Type', 'application/json')
-                .send({
-                        'username': usuario,
-                        'senha': senha
-                })
+                .send(bodyLogin)
                 
                 return respostaLogin.body.token
 
 }
 
-export {
+module.exports = {
     obterToken
 }
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,24 +1,28 @@
 const request = require('supertest'); //serve para interagir com a API
-const {expect} = require('chai') //para validar que o teste funciona como deveria
-require('dotenv').config()
-const { obterToken} = require('../helpers/autenticacao'); //importa a função para obter o token
+const { expect } = require('chai'); //para validar que o teste funciona como deveria
+require('dotenv').config();
+const { obterToken } = require('../helpers/autenticacao'); //importa a função para obter o token
+const postTransferencias = require('../fixtures/postTransferencias.json'); //importa o arquivo JSON com os dados da transferência
 
 describe('Transferencias', () => {
     describe('POST /transferencia', () => {
+        let token;
+
+        beforeEach(async () => {
+            token = await obterToken('julio.lima', '123456');
+            // O token é obtido antes de cada teste para garantir que ele esteja atualizado  
+            // Aqui você pode adicionar código que deve ser executado antes de cada teste, se necessário
+        });
+
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10 reais', async () => {
             // Capturar o token
-             const token = await obterToken('julio.lima', '123456');
+             const bodytransferencias = {...postTransferencias}
 
             const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-type', 'application/json')
-            .set('Authorization', 'Bearer ' + token)
-            .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                })
+            .set('Authorization', `Bearer  ${token}`)
+            .send(bodytransferenc)
 
             expect(resposta.status).to.equal(201);
           
@@ -26,18 +30,14 @@ describe('Transferencias', () => {
 
         it('Deve retornar 422 quando o valor for abaixo de 10 reais', async () => {
             // Capturar o token
-            const token = await obterToken('julio.lima', '123456');
-
+             const bodytransferencias = {...postTransferencias}
+            bodytransferencias.valor = 7; // Modifica o valor para abaixo de 10 reais
+            
             const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-type', 'application/json')
-            .set('Authorization', 'Bearer ' + token)
-            .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 7,
-                    token: ""
-                })
+            .set('Authorization', `Bearer ${token}`)
+            .send( bodytransferencias)
 
             expect(resposta.status).to.equal(422);
         
